a1021c72472b1b9981b75ff3f1df882bc4234030
Thiago Galvani <thiagopaiva99@gmail.com>
2021-09-30 19:00:06 -0300

    creating a simple nodejs server

---
+++ simple-web-server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(9000, () => {
+  console.log('Up and running!');
+});
